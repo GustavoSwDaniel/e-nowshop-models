28dd3ad384a7483587153bce5ee75d895e0c1ca5
GustavoSwDaniel <gustavodanieldetoledo@gmail.com>
2022-11-28 03:42:50 -0300

    feat: new models

--- enowshop_models/models/employees_phones.py
+++ enowshop_models/models/employees_phones.py
@@ -1,19 +1,21 @@
+
 from sqlalchemy import Column, Enum, ForeignKey, Integer, String, DateTime, func, event
 
 from enowshop_models.base import Base
 from enowshop_models.helpers.helpers import generate_data
+from sqlalchemy.dialects.postgresql import ENUM
 
 
-class UsersPhones(Base):
-    __tablename__ = 'users_phones'
+class EmployeesPhones(Base):
+    __tablename__ = 'employees_phones'
 
     id = Column(Integer, primary_key=True)
-    type = Column(Enum('Cell', 'Telephone', name='PhoneTypes'))
+    type = Column(ENUM('Cell', 'Telephone', name='PhoneTypesEmployees'))
     number = Column(String(12))
     created_at = Column(DateTime)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
 
-    user_id = Column(Integer, ForeignKey('users.id'))
+    employees_id = Column(Integer, ForeignKey('employees.id'))
 
 
-event.listen(UsersPhones, 'before_insert', generate_data)
+event.listen(EmployeesPhones, 'before_insert', generate_data)
