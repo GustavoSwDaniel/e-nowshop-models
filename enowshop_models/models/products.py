9dc795d7e8ad5021b4f488de5d57dacd0052abdf
gustavo.toledo <gustavo.toledo@visie.com.br>
2023-04-01 19:54:20 -0300

    feature: update

--- enowshop_models/models/products.py
+++ enowshop_models/models/products.py
@@ -18,7 +18,7 @@ class Products(Base):
     market = Column(Integer())
     unity = Column(Integer())
     image_url = Column(String(200))
-    infos = Column(JSON, nuallable=True)
+    infos = Column(JSON)
     created_at = Column(DateTime)
     updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
     promotion_id = Column(Integer, ForeignKey('promotion.id'), nullable=True)
