0873fa59d169bd23636e1688783cdb1cfdd7f9ee
gustavo.toledo <gustavo.toledo@visie.com.br>
2023-04-01 18:35:20 -0300

    version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 
-VERSION = '0.4.24'
+VERSION = '0.4.25'
 DESCRIPTION = 'e-nowshop-models'
 LONG_DESCRIPTION = 'Models of database of college project'
 #
